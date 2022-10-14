99138460be03dfe2a01aabd40a47e123adaf4f8d
Haejin Hwang <hhwang24@my.bcit.ca>
2022-10-13 18:08:55 -0700

    External Loop

---
+++ src/NestedLoop_2.java
@@ -0,0 +1,32 @@
+public class NestedLoop_2 {
+
+	public static void main(String[] args) {
+		
+		//ingredients! Hip and Hurray string Array!
+		String[] chantComp = {"Hip ","Hurray"};
+		
+		//How many hips do you want to repeat?
+		int hipTimes = 5;
+		
+		//How many singleLines do you want to repeat?
+		int LineTimes = 2;
+		
+		//Nice boxes to add string friends!
+		String hipChant = "";	
+		String singleLine = "";
+
+		for(int i=0;i<LineTimes;i++){
+			
+			for(int j=0;j<hipTimes;j++){
+				//each loop, nice and warm "Hip " is added!!
+				hipChant = hipChant+chantComp[0];	
+				}
+			//Once 3 hips are ready, just add "Hurray" 
+			//System.out.println(singleLine);
+			singleLine = hipChant + chantComp[1];
+			//empty the hipChant so that it doesn't accumulate the previous values
+			hipChant= "";
+			System.out.println(singleLine);
+			}
+		}
+}
